18ba23e55b905d90fbf80453ddf8c2e47dd080e6
karan8404 <kkawathia8404@gmail.com>
2024-07-25 23:53:26 +0530

    Improve card styling and add text color to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ export const Home = () => {
 
         <div className="card bg-green-800 w-72 md:w-96 shadow-xl hover:shadow-2xl transform hover:-translate-y-3 transition duration-300">
           <div className="card-body">
-            <h2 className="card-title">Annual Sports Day</h2>
-            <p> Celebrating Excellence in Sports</p>
+            <h2 className="card-title text-white">Annual Sports Day</h2>
+            <p className="text-white"> Celebrating Excellence in Sports</p>
           </div>
           <figure>
             <img src="/annual-sports-day.jpg" alt="annual sports day" className="w-full h-48 object-cover" />
@@ -18,8 +18,8 @@ export const Home = () => {
 
         <div className="card bg-yellow-800 w-72 md:w-96 shadow-xl  hover:shadow-2xl transform hover:-translate-y-3 transition duration-300">
           <div className="card-body">
-            <h2 className="card-title">Cultural Fest</h2>
-            <p>Embracing Diversity and Creativity</p>
+            <h2 className="card-title text-white">Cultural Fest</h2>
+            <p className="text-white">Embracing Diversity and Creativity</p>
           </div>
           <figure>
             <img src="/cultural_fest.jpeg" alt="cultural fest" className="w-full h-48 object-cover" />
@@ -28,8 +28,8 @@ export const Home = () => {
 
         <div className="card bg-red-800 w-72 md:w-96 shadow-xl  hover:shadow-2xl transform hover:-translate-y-3 transition duration-300">
           <div className="card-body">
-            <h2 className="card-title">Science Exhibition</h2>
-            <p>Showcasing Student Innovations</p>
+            <h2 className="card-title text-white">Science Exhibition</h2>
+            <p className="text-white">Showcasing Student Innovations</p>
           </div>
           <figure>
             <img src="/science_exhibition.jpg" alt="science exhibition" className="w-full h-48 object-cover object-top" />
